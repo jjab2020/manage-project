82f6649d1912c8a6c2728c3aedf14daa0650c4dc
jabrane <jabrane.pro@gmail.com>
2019-04-12 22:34:46 -0400

    Add placeholder text and Translations

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use App\Form\Type\RolesType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('password')
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter Username'
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter Email'
+                ]
+            ])
+            ->add('password', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter Password'
+                ]
+            ])
             ->add('isActive')
             ->add('roles', RolesType::class, [
                 'placeholder' => 'Choose a role option',
